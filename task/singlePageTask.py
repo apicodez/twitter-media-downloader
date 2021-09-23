b7f7f1f6d065814429fbc009affa1f7e052ffd81
mengzonefire <44224240+mengzonefire@users.noreply.github.com>
2021-09-23 16:10:41 +0800

    update

--- task/singlePageTask.py
+++ task/singlePageTask.py
@@ -1,7 +1,11 @@
 '''
 Author: mengzonefire
 Date: 2021-09-21 09:18:34
-LastEditTime: 2021-09-21 09:18:34
+LastEditTime: 2021-09-23 16:08:13
 LastEditors: mengzonefire
-Description: 
+Description: 单推文媒体爬取任务类
 '''
+
+class SinglePageTask:
+    def __init__(self, pageId):
+        pass
--- task/userMediaTask.py
+++ task/userMediaTask.py
@@ -1,7 +1,11 @@
 '''
 Author: mengzonefire
 Date: 2021-09-21 09:19:02
-LastEditTime: 2021-09-21 11:00:55
+LastEditTime: 2021-09-23 16:07:42
 LastEditors: mengzonefire
-Description: 
+Description: 推主推文批量爬取任务类
 '''
+
+class UserMediaTask:
+    def __init__(self, userName):
+        pass
